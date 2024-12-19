756203f0906a650ac341f2080fd919f33b258c85
Mark Peters <mapeters101@earthlink.net>
2024-12-19 13:39:54 -0500

    Work on lesson049.

---
+++ lesson049.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int number1{ 2 };
+	int number2{ 7 };
+	int result = number1 + number2;
+	cout << "result : " << result << endl << endl;
+
+	result = number2 - number1;
+	cout << "result : " << result << endl << endl;
+
+	result = number1 - number2;
+	cout << "result : " << result << endl << endl;
+
+	result = number2 * number1;
+	cout << "result : " << result << endl << endl;
+
+	result = number2 / number1;
+	cout << "result : " << result << endl << endl;
+
+	result = number2 % number1;
+	cout << "result : " << result << endl << endl;
+
+	result = 31 % 10;
+	cout << "result : " << result << endl << endl;
+
+	return 0;
+}
+*/
