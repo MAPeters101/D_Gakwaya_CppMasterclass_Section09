806a68104bee3fb97bc522b62ed1da5347db678f
Mark Peters <mapeters101@earthlink.net>
2024-12-19 17:22:07 -0500

    Work on lesson053.

---
+++ lesson053.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int number1{ 45 };
+	int number2{ 60 };
+
+	cout << "number1 : " << number1 << endl;
+	cout << "number2 : " << number2 << endl << endl;
+
+	cout << "Comparing variables" << endl;
+	cout << boolalpha;
+	//cout << "number1 < number 2 : " << number1 < number2 << endl;
+	cout << "number1 < number 2 : " << (number1 < number2) << endl;
+	cout << "number1 <= number 2 : " << (number1 <= number2) << endl;
+	cout << "number1 > number 2 : " << (number1 > number2) << endl;
+	cout << "number1 >= number 2 : " << (number1 >= number2) << endl;
+	cout << "number1 == number 2 : " << (number1 == number2) << endl;
+	cout << "number1 != number 2 : " << (number1 != number2) << endl << endl;
+
+	cout << "Store comparison result and use it later: " << endl;
+	bool result = (number1 == number2);
+	cout << number1 << " == " << number2 << " : " << result << endl;
+
+	return 0;
+}
+*/
