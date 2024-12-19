ab8b91b4f60b4864279fe230eff1c7dc7c7a9bd8
Mark Peters <mapeters101@earthlink.net>
2024-12-19 17:30:59 -0500

    Work on lesson054 to 9:20.

---
+++ lesson054.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+	bool a{ true };
+	bool b{ false };
+	bool c{ true };
+
+	cout << boolalpha;
+	cout << "a : " << a << endl;
+	cout << "b : " << b << endl;
+	cout << "c : " << c << endl << endl;
+
+
+	cout << "Basic AND operations" << endl;
+	cout << "a  && b : " << (a && b) << endl;
+	cout << "a  && c : " << (a && c) << endl;
+	cout << "a  && b && c : " << (a && b && c) << endl << endl;
+
+
+	return 0;
+}
+
+
