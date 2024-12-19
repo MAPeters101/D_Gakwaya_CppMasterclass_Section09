ac3830aed7b5dbde4ed0d1298e7e05e50142865a
Mark Peters <mapeters101@earthlink.net>
2024-12-19 17:57:12 -0500

    Work on lesson055 to 16:50.

---
+++ lesson055.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	cout << "Hello";
+	cout << "World";
+	cout << endl;
+
+	cout << "--------------------" << endl;
+	cout << "Hello" << endl;
+	cout << "World" << endl;
+	cout << endl;
+
+	cout << "Hello\n";
+	cout << "World\n";
+	cout << endl;
+
+	return 0;
+}
+
+
