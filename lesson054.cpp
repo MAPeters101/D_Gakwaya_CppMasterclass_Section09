dbc2e13438cc69f4fe2cb3d0806e1368ea0aa037
Mark Peters <mapeters101@earthlink.net>
2024-12-19 17:33:49 -0500

    Work on lesson054 to 11:30.

--- lesson054.cpp
+++ lesson054.cpp
@@ -14,11 +14,15 @@ int main() {
 	cout << "b : " << b << endl;
 	cout << "c : " << c << endl << endl;
 
-
 	cout << "Basic AND operations" << endl;
 	cout << "a  && b : " << (a && b) << endl;
 	cout << "a  && c : " << (a && c) << endl;
 	cout << "a  && b && c : " << (a && b && c) << endl << endl;
+
+	cout << "Basic OR operations" << endl;
+	cout << "a  || b : " << (a || b) << endl;
+	cout << "a  || c : " << (a || c) << endl;
+	cout << "a  || b || c : " << (a || b || c) << endl << endl;
 
 
 	return 0;
