29db719267464e199fdfefd20cab97a23b9855ac
Mark Peters <mapeters101@earthlink.net>
2024-12-19 17:51:56 -0500

    Complete lesson054.

--- lesson054.cpp
+++ lesson054.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-
+/*
 int main() {
 	bool a{ true };
 	bool b{ false };
@@ -30,10 +30,26 @@ int main() {
 	cout << "!c : " << !c << endl << endl;
 
 	cout << "Combining logical operators" << endl;
-	cout << "(!(a && b) || c) : " << (!(a && b) || c) << endl;
+	cout << "(!(a && b) || c) : " << (!(a && b) || c) << endl << endl;
 
+
+	cout << "Relational and logical operations on integers:" << endl;
+	int d{ 45 };
+	int e{ 20 };
+	int f{ 11 };
+	cout << "d : " << d << endl;
+	cout << "e : " << e << endl;
+	cout << "f : " << f << endl << endl;
+
+	cout << "(d > e) && (d > f) : " << ((d > e) && (d > f)) << endl;
+	cout << "(d==e) || (e <= f) : " << ((d == e) || (e <= f)) << endl;
+	cout << "(d < e) || (d > f) : " << ((d < e) || (d > f)) << endl;
+	cout << "(f > e) || (d < f) : " << ((f > e) || (d < f)) << endl;
+	cout << "(d > f) && (f <= d) : " << ((d > f) && (f <= d)) << endl;
+	cout << "(d > e) && (d <= f) : " << ((d > e) && (d <= f)) << endl;
+	cout << "(!a) && (d == e) : " << ((!a) && (d == e)) << endl;
 
 	return 0;
 }
-
+*/
 
