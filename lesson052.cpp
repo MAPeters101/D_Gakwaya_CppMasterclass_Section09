da0bd09bf03c3b54585d23f6cbb4eb963dfff217
Mark Peters <mapeters101@earthlink.net>
2024-12-19 17:12:39 -0500

    Work on lesson052.

---
+++ lesson052.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int value{ 45 };
+
+	cout << "The value is : " << value << endl;
+	value += 5;
+	cout << "The value after += 5 is : " << value << endl << endl;
+
+	value -= 5;
+	cout << "The value after -= 5 is : " << value << endl << endl;
+
+	value *= 2;
+	cout << "The value after *= 2 is : " << value << endl << endl;
+
+	value /= 3;
+	cout << "The value after /= 3 is : " << value << endl << endl;
+
+	value %= 11;
+	cout << "The value after %= 11 is : " << value << endl << endl;
+
+	return 0;
+}
+*/
+
