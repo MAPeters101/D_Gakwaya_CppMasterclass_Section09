e8328dfc8088b060d749418751ea00a2fce1198c
Mark Peters <mapeters101@earthlink.net>
2024-12-20 18:38:37 -0500

    Work on lesson057 to 11:30.

--- lesson057.cpp
+++ lesson057.cpp
@@ -19,6 +19,17 @@ int main() {
 	cout << "3 ^ 4 is : " << std::pow(3, 4) << endl;
 	cout << "9 ^ 3 is : " << std::pow(9,3) << endl;
 
+	// log : reverse function of pow.  if 2^3 = 8, log 8 in base 2 = 3.  Log is like as
+	// to which exponent should we elevate 2 to get eight?  Log, by default computes the
+	// in base e.  There also is another function which uses base 10 called log10
+
+	// Try the reverse operation of e^4 = 54.59, it will be log 54.59 in base e = ?
+	cout << "Log; to get 54.59, you would elevate e to the power of : "
+		<< std::log(54.59) << endl;
+
+	//log10, 10 ^ 4 = 10000, to get 10km you'd need to elevate 10 to the power of
+	cout << "To get 10000, you'd need to elevate 10 to the power of : "
+		<< std::log10(10000) << endl;
 
 	return 0;
 }
