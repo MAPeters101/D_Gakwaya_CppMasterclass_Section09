5b9168503bf55c2f4f72dddca3b731a901f13955
Mark Peters <mapeters101@earthlink.net>
2024-12-20 16:03:07 -0500

    Work on lesson055 to 20:45.

--- lesson055.cpp
+++ lesson055.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -19,6 +20,41 @@ int main() {
 	cout << "===============================" << endl << endl;
 
 	cout << "This is a nice message..." << endl << flush;
+	cout << "===============================" << endl << endl;
+
+
+	cout << "Unformatted table : " << endl;
+	cout << "Lastname" << " " << "Firstname" << "Age" << endl;
+	cout << "Daniel" << " " << "Gray" << " 25" << endl;
+	cout << "Stanley" << " " << "Woods" << " 33" << endl;
+	cout << "Jordan" << " " << "Parker" << " 45" << endl;
+	cout << "Joe" << " " << "Ball" << " 21" << endl;
+	cout << "Josh" << " " << "Carr" << " 27" << endl;
+	cout << "Izaiah" << " " << "Robinson" << " 29" << endl;
+	cout << endl;
+
+	cout << "Formatted table : " << endl;
+	cout << std::setw(10) << "Lastname" << std::setw(10) << "Firstname" << std::setw(5) << "Age" << endl;
+	cout << std::setw(10) << "Daniel" << std::setw(10) << "Gray" << std::setw(5) << " 25" << endl;
+	cout << std::setw(10) << "Stanley" << std::setw(10) << "Woods" << std::setw(5) << " 33" << endl;
+	cout << std::setw(10) << "Jordan" << std::setw(10) << "Parker" << std::setw(5) << " 45" << endl;
+	cout << std::setw(10) << "Joe" << std::setw(10) << "Ball" << std::setw(5) << " 21" << endl;
+	cout << std::setw(10) << "Josh" << std::setw(10) << "Carr" << std::setw(5) << " 27" << endl;
+	cout << std::setw(10) << "Izaiah" << std::setw(10) << "Robinson" << std::setw(5) << " 29" << endl;
+	cout << endl;
+
+	int col_width{ 14 };
+	int col_width2{ 7 };
+
+	cout << "Formatted table with variables: " << endl;
+	cout << std::setw(col_width) << "Lastname" << std::setw(col_width) << "Firstname" << std::setw(col_width2) << "Age" << endl;
+	cout << std::setw(col_width) << "Daniel" << std::setw(col_width) << "Gray" << std::setw(col_width2) << " 25" << endl;
+	cout << std::setw(col_width) << "Stanley" << std::setw(col_width) << "Woods" << std::setw(col_width2) << " 33" << endl;
+	cout << std::setw(col_width) << "Jordan" << std::setw(col_width) << "Parker" << std::setw(col_width2) << " 45" << endl;
+	cout << std::setw(col_width) << "Joe" << std::setw(col_width) << "Ball" << std::setw(col_width2) << " 21" << endl;
+	cout << std::setw(col_width) << "Josh" << std::setw(col_width) << "Carr" << std::setw(col_width2) << " 27" << endl;
+	cout << std::setw(col_width) << "Izaiah" << std::setw(col_width) << "Robinson" << std::setw(col_width2) << " 29" << endl;
+	cout << endl;
 
 	return 0;
 }
