918adf457c22e02e8a43e0bf05ca8dcc481b19c2
Mark Peters <mapeters101@earthlink.net>
2024-12-21 15:17:02 -0500

    Work on assignment03.

---
+++ Assignment03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	double length{ 0 }, width{ 0 }, height{ 0 }, base_area{ 0 }, volume{ 0 };
+	cout << "Welcome to box calculator. Please type in length, width and height information :" << endl;
+	cout << "length : ";
+	cin >> length;
+	cout << "width : ";
+	cin >> width;
+	cout << "height : ";
+	cin >> height;
+	base_area = width * length;
+	volume = base_area * height;
+	cout << "The base area is : " << base_area << endl;
+	cout << "The volume is : " << volume;
+
+	return 0;
+}
+*/
