7a22cc1bbf4da17b50e3f9111797977e760da1f0
Mark Peters <mapeters101@earthlink.net>
2024-12-21 14:48:20 -0500

    Work on lesson058.

---
+++ lesson058.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	short int var1{ 10 };
+	short int var2{ 20 };
+
+	char var3{ 40 };
+	char var4{ 50 };
+
+	cout << "sizeof var1 : " << sizeof(var1) << endl;
+	cout << "sizeof var2 : " << sizeof(var2) << endl;
+	cout << "sizeof var3 : " << sizeof(var3) << endl;
+	cout << "sizeof var4 : " << sizeof(var4) << endl;
+
+	auto result1 = var1 + var2;
+	auto result2 = var3 + var4;
+
+	cout << "size of result1 : " << sizeof(result1) << endl;
+	cout << "size of result2 : " << sizeof(result2) << endl;
+
+	return 0;
+}
+*/
+
