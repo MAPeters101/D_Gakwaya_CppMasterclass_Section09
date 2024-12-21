21ebbf7755b0c5b33f378e6cf614ce930226961e
Mark Peters <mapeters101@earthlink.net>
2024-12-21 14:59:12 -0500

    Work on assignment02.

---
+++ Assignment02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	double celsius{ 0 };
+	cout << "Please enter a degree value in Celsius : " << endl;
+	cin >> celsius;
+	double fahrenheit{ ((9.0 / 5) * celsius + 32) };
+	cout << celsius << " Celsius is " << fahrenheit << " Farenheit";
+
+	return 0;
+}
+*/
